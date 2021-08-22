edaabe504cce96912d1b78253ef440bd0d86d8d9
Maic Miller <maicmiller42@gmail.com>
2021-08-22 16:58:57 -0300

    test():flow validate user successfully

--- loginSelenide/src/test/java/com/github/maicmiller/Tests/LoginTests.java
+++ loginSelenide/src/test/java/com/github/maicmiller/Tests/LoginTests.java
@@ -2,6 +2,7 @@ package com.github.maicmiller.Tests;
 
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -9,22 +10,33 @@ import static com.codeborne.selenide.WebDriverRunner.isChrome;
 
 public class LoginTests {
 
-    String userName = "*******";
-    String userPass = "*******";
+    String userName = "******";
+    String userPass = "******";
 
+    /*
+    * Cenário de ‘login’ com sucesso.
+    * * Usar o seletor CSS pelo ID e Propriedade de um element;
+    * * Preencher Formulário simples com Selenide;
+    * * Clicar em botão;
+    * * Validar conteúdo esperado na tela;
+    * */
     @Test
-    public void shouldSeeLoggedUser(){
+    public void shouldSeeLoggedUser() {
+        //Defino o navegador
         isChrome();
+        //Acesso ha página principal
         open("https://www.netflix.com/br/");
-
         $(byText("Entrar")).click();
 
+        //Preencho o formulário
         $("#id_userLoginId").setValue(userName);
         $("#id_password").setValue(userPass);
-        $(byText("Sign In")).click();
 
-        //Validar página Users/Kids
+        //Submeto o formulário
+        $("button[data-uia=login-submit-button]").click();
+
+        // Valida área logada de GERENCIAR PERFIS - $(".profile-gate-label");
+        $(".profile-gate-label").shouldHave(text("Quem está assistindo?"));
 
     }
-
 }
